c77c1bf5869462d19ce75a653ccd41b389bb1b03
Любимов Дмитрий <142087671+LyubimovDS@users.noreply.github.com>
2024-05-27 16:04:31 +0300

    Same comment

--- src/test/java/Testing/methods.java
+++ src/test/java/Testing/methods.java
@@ -8,8 +8,8 @@ public class methods {
     public void selectOption(String field, String element){
         $(field).click();
 
-        $x("//option[text() = \"" + element + "\"]").click();
-        //$$("option").find(text("Python")).click();
+        //$x("//option[text() = \"" + element + "\"]").click();
+        $$("option").find(text(element)).click();
 
 
         $(field).shouldHave(text(element));
